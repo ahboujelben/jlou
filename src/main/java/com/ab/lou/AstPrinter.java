7596e566dd7c846a6d49fb4b34af095c2725b80a
Ahmed Boujelben <ahmed.hamdi.boujelben@gmail.com>
2022-07-30 19:32:36 +0100

    added Expr class generator

---
+++ src/main/java/com/ab/lou/AstPrinter.java
@@ -0,0 +1,43 @@
+package com.ab.lou;
+
+class AstPrinter implements Expr.Visitor<String> {
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme,
+                expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null)
+            return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(print(expr));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+}
